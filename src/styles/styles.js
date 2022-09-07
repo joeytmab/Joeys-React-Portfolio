2ce251e811adbfbe73a6a2224e17c52def65cda2
Joey Binas <joseph.t.binas@gmail.com>
2022-09-07 03:10:44 -0700

    added styles

---
+++ src/styles/styles.js
@@ -0,0 +1,33 @@
+const styles = {
+
+    hero: {
+        position: 'relative',
+        width: '100%',
+        height: '100vh',
+        alignItems: 'center',
+        justifyContent: 'center',
+        background: '#b7b7b7'
+    },
+    span: {
+        color:' #414a7a',
+        fontWeight: 'bold'
+    },
+    aboutMe: {
+        marginLeft: '5%',
+        marginTop: '8%',
+        width: '40%',
+        height: '40%'
+    },
+    aboutMeH1: {
+        fontSize: '50px',
+        color:'rgb(59, 54, 54)',
+        marginBottom: '30px',
+    },
+    aboutMeP: {
+        color:' #555',
+        fontSize: '18px',
+        fontFamily:'Arial, Helvetica, sans-serif',
+        lineHeight: '2em',
+        marginBottom: '10px'
+    }
+}
