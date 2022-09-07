1555e1c9d694bd8534e1164d82d603310628f292
Joey Binas <joseph.t.binas@gmail.com>
2022-09-07 03:13:31 -0700

    created footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <footer>
+        <div className="footer" id="copyright">
+        <h5><i className="fa-regular fa-copyright"></i>  JTMAB 2022</h5>
+        </div>
+     </footer>
+    );
+}
+
+export default Footer;
